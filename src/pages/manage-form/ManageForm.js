c274c1b1b560213151934927e9d10253d7b9d529
0teklee <leetekwoo@gmail.com>
2022-04-18 19:15:40 +0900

    [ADD] manage-form 브랜치 생서

--- src/pages/manage-form/ManageForm.js
+++ src/pages/manage-form/ManageForm.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+// import Header from '../../components/header/Header';
 
 function ManageForm() {
-  return <div>ManageForm</div>;
+  return (
+    <Wrapper>
+      {/* <Header /> */}
+      <Title>방내놓기</Title>
+    </Wrapper>
+  );
 }
 
+const Wrapper = styled.section``;
+const Title = styled.h1`
+  font-size: 2rem;
+  text-align: center;
+`;
 export default ManageForm;
