383cb46b784d6c2189bbecc8d9e80ecafaac3b43
haeyun jun <haleyjun9@gmail.com>
2022-04-18 20:15:38 +0900

    commit before pull develop -- settings.json

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 function Main() {
-  return <div>Main</div>;
+//   const Button = styled.a`
+//   /* This renders the buttons above... Edit me! */
+//   display: inline-block;
+//   border-radius: 3px;
+//   padding: 0.5rem 0;
+//   margin: 0.5rem 1rem;
+//   width: 11rem;
+//   background: transparent;
+//   color: white;
+//   border: 2px solid white;
+
+//   /* The GitHub button is a primary button
+//    * edit this to target it specifically! */
+//   ${props => props.primary && css`
+//     background: white;
+//     color: black;
+//   `}
+// `
+  return (
+    <div>
+hello
+    </div>
+  )
 }
 
 export default Main;
