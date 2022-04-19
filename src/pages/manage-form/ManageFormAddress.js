dc273e6a3aad575498b2b3b2f8c85f3f43a9ef43
0teklee <leetekwoo@gmail.com>
2022-04-19 17:07:36 +0900

    [ADD] 위치정보 컴포넌트 레이아웃 추가

--- src/pages/manage-form/ManageFormAddress.js
+++ src/pages/manage-form/ManageFormAddress.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsCheck } from 'react-icons/bs';
 import styled from 'styled-components';
 import DaumPostcode from 'react-daum-postcode';
 
 function ManageFormAddress() {
+  const [check, setCheck] = useState(false);
+  const handleCheck = e => {
+    setCheck(prev => !prev);
+  };
   return (
     <Wrapper>
       <Title>
@@ -19,10 +24,7 @@ function ManageFormAddress() {
               도로명, 건물명, 지번에 대해 통합검색이 가능합니다.
             </SearchNotice>
             <SearchAddressBox>
-              <TextInput
-                placeholder="예)번동 10-1, 강북구 번동"
-                marginRight="15px"
-              />
+              <TextInput placeholder="예)번동 10-1, 강북구 번동" />
               <ButtonInput value="주소검색" />
             </SearchAddressBox>
             <BorderBox>
@@ -30,9 +32,26 @@ function ManageFormAddress() {
               <div>지 번 : </div>
             </BorderBox>
             <FlexDiv>
-              <TextInput />
-              <TextInput />
+              <FlexDiv check={check}>
+                <TextInput placeholder="예 ) 101동" check={check} />
+                <DetailAdressBox marginRight="5px">동</DetailAdressBox>
+              </FlexDiv>
+              <FlexDiv width={check}>
+                <TextInput placeholder="예 ) 101호" />
+                <DetailAdressBox>호</DetailAdressBox>
+              </FlexDiv>
             </FlexDiv>
+            <Input>
+              <div className="inputCheckbox" onClick={handleCheck}>
+                <input id="inputCheck" type="checkbox" checked={check} />
+                <label htmlFor="inputCheck">
+                  <BsCheck size="20px" color="#fff" />
+                </label>
+                <p className="notice">
+                  등본에 동정보가 없는 경우 클릭해주세요.
+                </p>
+              </div>
+            </Input>
           </InputInnerWrapper>
 
           <MapWrapper>지도가 들어갈 자리</MapWrapper>
@@ -79,7 +98,6 @@ const RowHead = styled.div`
   justify-content: center;
   width: 12%;
   text-align: center;
-  border-bottom: 1px solid rgb(226, 226, 226);
   border-right: 1px solid rgb(226, 226, 226);
   background: #fdfdfd;
 `;
@@ -93,7 +111,9 @@ const AddressInputWrapper = styled.div`
 `;
 
 const FlexDiv = styled.div`
-  display: flex;
+  display: ${props => (props.check ? 'none' : 'flex')};
+  width: ${props => (props.width ? '' : '100%')};
+  align-items: center;
 `;
 
 const InputInnerWrapper = styled.div`
@@ -121,12 +141,35 @@ const BorderBox = styled.div`
   border: 1px solid rgb(226, 226, 226);
 `;
 
+const Input = styled.div`
+  .inputCheckbox {
+    margin: 15px 0px;
+    position: relative;
+    cursor: pointer;
+    input[type='checkbox'] {
+      display: none;
+    }
+    .notice {
+      display: inline-block;
+      margin-left: 30px;
+    }
+    label {
+      border: 1px solid rgb(226, 226, 226);
+      position: absolute;
+      bottom: -5px;
+    }
+    input[type='checkbox']:checked + label {
+      background: rgb(50, 108, 249);
+    }
+  }
+`;
+
 const TextInput = styled.input.attrs(props => ({
   type: 'text',
   name: props.name,
 }))`
   display: inline-block;
-  margin-right: 15px;
+  margin-right: ${props => props.marginRight};
   width: 100%;
   height: 46px;
   padding: 0px 16px;
@@ -134,12 +177,39 @@ const TextInput = styled.input.attrs(props => ({
   font-size: 14px;
   border: 1px solid rgb(223, 223, 223);
   border-radius: 2px;
+  &:focus {
+    outline: none;
+    border: 1px solid rgb(50, 108, 249);
+  }
+`;
+
+const DetailAdressBox = styled.div`
+  display: flex;
+  height: 46px;
+  margin-right: ${props => props.marginRight};
+  padding: 0px 16px;
+  border: 1px solid rgb(226, 226, 226);
+  align-items: center;
 `;
 
 const ButtonInput = styled.input.attrs(props => ({
   type: 'button',
   name: props.name,
-}))``;
+}))`
+  -webkit-box-flex: 1;
+  flex-grow: 1;
+  margin-left: 15px;
+  height: 46px;
+  vertical-align: middle;
+  color: rgb(255, 255, 255);
+  font-size: 14px;
+  font-weight: 500;
+  padding: 1rem;
+  border: 0px;
+  border-radius: 3px;
+  background-color: rgb(35, 45, 74);
+  cursor: pointer;
+`;
 
 const MapWrapper = styled.div`
   width: 40%;
