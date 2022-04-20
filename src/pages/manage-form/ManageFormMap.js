0304fdaca82cb9e34d37fb3453e762e689b84c4b
0teklee <leetekwoo@gmail.com>
2022-04-20 20:46:41 +0900

    [FIX] 주소 입력창 지도 수정..2

--- src/pages/manage-form/ManageFormMap.js
+++ src/pages/manage-form/ManageFormMap.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 function ManageFormMap({ Address = { Lat: 0, Lng: 0 } }) {
-  const container = useRef(null); //지도를 담을 영역의 DOM 레퍼런스
+  // const container = useRef(null); //지도를 담을 영역의 DOM 레퍼런스
+  const container = document.getElementById('id');
   const { kakao } = window;
   const { Lat, Lng } = Address;
   const options = {
@@ -9,25 +10,18 @@ function ManageFormMap({ Address = { Lat: 0, Lng: 0 } }) {
     level: 3, //지도의 레벨(확대, 축소 정도)
     draggable: false,
   };
-  useEffect(() => {
-    new kakao.maps.Map(container.current, options); //지도 생성 및 객체 리턴
-    return () => {};
-  }, []);
-
-  const map = new kakao.maps.Map( , options)
+  // useEffect(() => {
+  //   new kakao.maps.Map(container.current, options); //지도 생성 및 객체 리턴
+  //   return () => {};
+  // }, []);
+  const map = new kakao.maps.Map(container, options);
   const geocoder = new kakao.maps.services.Geocoder();
   const marker = new kakao.maps.Marker({
     position: new kakao.maps.LatLng(Lat, Lng),
     map: container,
   });
 
-  return (
-    <div
-      className="map"
-      style={{ width: '100%', height: '100%' }}
-      ref={container}
-    />
-  );
+  return <div id="map" style={{ width: '100%', height: '100%' }} />;
 }
 
 export default ManageFormMap;
