8815c3099e9496b47bdecb28cb1e2f023a317e5c
0teklee <leetekwoo@gmail.com>
2022-04-20 20:42:37 +0900

    [FIX] 주소 입력창 지도 수정

--- src/pages/manage-form/ManageFormMap.js
+++ src/pages/manage-form/ManageFormMap.js
@@ -1,31 +1,30 @@
 import React, { useEffect, useRef } from 'react';
-
-const options = {
-  //지도를 생성할 때 필요한 기본 옵션
-  center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-  level: 3, //지도의 레벨(확대, 축소 정도)
-};
-
-function ManageFormMap({ Width, Height, Lat, Lng }) {
+function ManageFormMap({ Address = { Lat: 0, Lng: 0 } }) {
   const container = useRef(null); //지도를 담을 영역의 DOM 레퍼런스
-
+  const { kakao } = window;
+  const { Lat, Lng } = Address;
+  const options = {
+    //지도를 생성할 때 필요한 기본 옵션
+    center: new kakao.maps.LatLng(Lat, Lng), //지도의 중심좌표.
+    level: 3, //지도의 레벨(확대, 축소 정도)
+    draggable: false,
+  };
   useEffect(() => {
-    new window.kakao.maps.Map(container.current, options); //지도 생성 및 객체 리턴
+    new kakao.maps.Map(container.current, options); //지도 생성 및 객체 리턴
     return () => {};
   }, []);
 
-  const geocoder = new window.kakao.maps.services.Geocoder();
-  const marker = (Lat, Lng) => {
-    return new window.kakao.maps.Marker({
-      position: new window.kakao.maps.LatLng(Lat, Lng),
-      map: container,
-    });
-  };
+  const map = new kakao.maps.Map( , options)
+  const geocoder = new kakao.maps.services.Geocoder();
+  const marker = new kakao.maps.Marker({
+    position: new kakao.maps.LatLng(Lat, Lng),
+    map: container,
+  });
 
   return (
     <div
       className="map"
-      style={{ width: Width || '100%', height: Height || '100%' }}
+      style={{ width: '100%', height: '100%' }}
       ref={container}
     />
   );
