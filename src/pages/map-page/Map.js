785a5f03e9b5627b6500b1812a1f773506bd1c96
0teklee <leetekwoo@gmail.com>
2022-04-21 15:34:59 +0900

    [FIX] context 버그 수정

--- src/pages/map-page/Map.js
+++ src/pages/map-page/Map.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useRef, useContext } from 'react';
 import { markerdata } from './markerData';
-import { MarkerContext, MarkerDispatch, MarkerDispatch } from './context';
-
-const Marker = useContext(MarkerContext);
-const MarkerDispatch = useContext(MarkerDispatch);
+import { MarkerContext, MarkerDispatch } from './context';
 
 function Map() {
+  const Marker = useContext(MarkerContext);
+  const markerDispatch = useContext(MarkerDispatch);
+
   useEffect(() => {
     fetch('지도 마커 GET URL', {
       method: 'GET',
       headers: {
         'Content-type': 'application/json',
-        LatLng: `${CurrentLatLong}`,
+        // LatLng: `${CurrentLatLong}`,
       },
     })
       .then(res => res.json)
@@ -29,7 +29,7 @@ function Map() {
       level: 7, //지도의 레벨(확대, 축소 정도)
     };
     const map = new kakao.maps.Map(container, options);
-
+    const bounds = map.getBounds();
     const clusterer = new kakao.maps.MarkerClusterer({
       map: map,
       averageCenter: true,
@@ -43,17 +43,6 @@ function Map() {
         position: new kakao.maps.LatLng(el.lat, el.lng),
       });
     });
-
-    // const zoomControl = new kakao.maps.ZoomControl();
-    // map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-    // kakao.maps.event.addListener(map, 'zoom_changed', function () {
-    //   const level = map.getLevel();
-    //   switch (level) {
-    //     case 1 || 2:
-    //       clusterer.addMarkers(marker);
-    //       return;
-    //   }
-    // });
 
     clusterer.addMarkers(marker);
   };
--- src/pages/map-page/context.js
+++ src/pages/map-page/context.js
@@ -1,6 +1,6 @@
 import { createContext, useReducer } from 'react';
 
-const initialMarker = {markers : [];};
+const initialMarker = { markers: [] };
 
 export const MarkerContext = createContext();
 export const MarkerDispatch = createContext();
@@ -8,18 +8,18 @@ export const MarkerDispatch = createContext();
 function markerReducer(state, action) {
   switch (action.type) {
     case 'GET_MARKERS':
-      return {markers : action.getMarkers};
+      return { markers: action.getMarkers };
   }
 }
 
 export function GlobalContextProvider({ children }) {
-  const [marker, markerDispatch] = useReducer(markerReducer, initialMarker)
+  const [marker, markerDispatch] = useReducer(markerReducer, initialMarker);
 
   return (
     <MarkerContext.Provider value={marker}>
       <MarkerDispatch.Provider value={markerDispatch}>
-          {children}
+        {children}
       </MarkerDispatch.Provider>
     </MarkerContext.Provider>
-  )
+  );
 }
