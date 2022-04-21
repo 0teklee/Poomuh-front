6a9f1e4cda0d887f972beb552f04693f0cc0c66d
0teklee <leetekwoo@gmail.com>
2022-04-21 17:50:15 +0900

    [FIX] Context 수정

--- src/pages/map-page/Map.js
+++ src/pages/map-page/Map.js
@@ -1,27 +1,57 @@
 import React, { useEffect, useRef, useContext } from 'react';
 import { markerdata } from './markerData';
-import { MarkerContext, MarkerDispatch } from './context';
+import { RealEstateContext, RealEstateContextDispatch } from './context';
 
 function Map() {
-  const Marker = useContext(MarkerContext);
-  const markerDispatch = useContext(MarkerDispatch);
+  const RealEstate = useContext(RealEstateContext);
+  const RealEstateDispatch = useContext(RealEstateContextDispatch);
+  const { kakao } = window;
+  let container = document.getElementById('map');
+  let options = {
+    center: new kakao.maps.LatLng(37.624915253753194, 127.15122688059974), //지도의 중심좌표.
+    level: 7, //지도의 레벨(확대, 축소 정도)
+  };
+
+  // 지도의 좌표 범위를 보내는 fetch 함수
+  const sendBounds = coords => {
+    console.log(coords);
+    // fetch('범위 내의 매물 GET URL', {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-type': 'application/json',
+    //   },
+    //   body: {
+    //     LatLng: `${coords}`,
+    //   },
+    // })
+    //   .then(res => res.json)
+    //   .then(data => MarkerDispatch({ type: 'GET_MARKERS', getMarkers: data }));
+  };
+
+  // 바뀐 좌표 범위 내부의 매물을 받아오는 fetch함수
+  useEffect(() => {
+    fetch('백엔드에서 좌표 범위 내의 매물을 요청하는 URI', { method: 'GET' })
+      .then(res => res.json())
+      .then(data =>
+        RealEstateDispatch({ type: 'GET_REAL_ESTATE', realEstate: data })
+      )
+      .then(
+        RealEstate.realEstate.map(el => {
+          return new kakao.maps.Marker({
+            position: new kakao.maps.LatLng(el.lat, el.lng),
+          });
+        })
+      );
+  }, [RealEstate.mapBounds]);
 
   useEffect(() => {
-    fetch('지도 마커 GET URL', {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json',
-        // LatLng: `${CurrentLatLong}`,
-      },
-    })
-      .then(res => res.json)
-      .then(data => MarkerDispatch({ type: 'GET_MARKERS', getMarkers: data }));
-  }, []);
+    sendBounds(RealEstate.mapBounds);
+  }, [RealEstate.mapBounds]);
+
   useEffect(() => {
     mapscript();
   }, []);
 
-  const { kakao } = window;
   const mapscript = () => {
     let container = document.getElementById('map');
     let options = {
@@ -29,7 +59,6 @@ function Map() {
       level: 7, //지도의 레벨(확대, 축소 정도)
     };
     const map = new kakao.maps.Map(container, options);
-    const bounds = map.getBounds();
     const clusterer = new kakao.maps.MarkerClusterer({
       map: map,
       averageCenter: true,
@@ -37,6 +66,8 @@ function Map() {
       minClusterSize: 1,
     });
 
+    // markerdata == Mock Data
+    // const marker = RealEstate.map(el => {
     const marker = markerdata.map(el => {
       return new kakao.maps.Marker({
         map: map,
@@ -45,8 +76,18 @@ function Map() {
     });
 
     clusterer.addMarkers(marker);
+    kakao.maps.event.addListener(map, 'zoom_changed', () => {
+      const bounds = map.getBounds();
+      RealEstateDispatch({ type: 'GET_BOUNDS', getBounds: bounds });
+    });
+    kakao.maps.event.addListener(map, 'dragend', () => {
+      const bounds = map.getBounds();
+      RealEstateDispatch({ type: 'GET_BOUNDS', getBounds: bounds });
+    });
+    kakao.maps.addListener(map, , () => {
+      map.setMarkerPosition()
+    })
   };
-
   return (
     <div>
       <div id="map" style={{ width: '100vw', height: '100vh' }} />
--- src/pages/map-page/context.js
+++ src/pages/map-page/context.js
@@ -1,25 +1,32 @@
 import { createContext, useReducer } from 'react';
 
-const initialMarker = { markers: [] };
+const initialRealEstate = { realEstate: [], mapBounds: {} };
 
-export const MarkerContext = createContext();
-export const MarkerDispatch = createContext();
+export const RealEstateContext = createContext();
+export const RealEstateContextDispatch = createContext();
 
-function markerReducer(state, action) {
+function realEstateReducer(state, action) {
   switch (action.type) {
-    case 'GET_MARKERS':
-      return { markers: action.getMarkers };
+    case 'GET_REAL_ESTATE':
+      return { ...state, realEstate: action.realEstate };
+    case 'GET_BOUNDS':
+      return { ...state, mapBounds: action.getBounds };
+    default:
+      throw new Error('UNKNOWN ACTION TYPE', action.type);
   }
 }
 
 export function GlobalContextProvider({ children }) {
-  const [marker, markerDispatch] = useReducer(markerReducer, initialMarker);
+  const [realEstate, markerDispatch] = useReducer(
+    realEstateReducer,
+    initialRealEstate
+  );
 
   return (
-    <MarkerContext.Provider value={marker}>
-      <MarkerDispatch.Provider value={markerDispatch}>
+    <RealEstateContext.Provider value={realEstate}>
+      <RealEstateContextDispatch.Provider value={markerDispatch}>
         {children}
-      </MarkerDispatch.Provider>
-    </MarkerContext.Provider>
+      </RealEstateContextDispatch.Provider>
+    </RealEstateContext.Provider>
   );
 }
