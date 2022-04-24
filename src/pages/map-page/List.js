b41c27853e284b339a2f34c78d6df07c48720b5a
0teklee <leetekwoo@gmail.com>
2022-04-24 14:38:40 +0900

    [ADD] map-list 컴포넌트 Context 추가

--- src/pages/map-page/List.js
+++ src/pages/map-page/List.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { RealEstateContext, RealEstateContextDispatch } from './context';
 
 function List() {
+  const RealEstate = useContext(RealEstateContext);
+  const RealEstateDispatch = useContext(RealEstateContextDispatch);
   return <div>List</div>;
 }
 
