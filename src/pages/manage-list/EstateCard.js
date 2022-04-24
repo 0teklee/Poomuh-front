e29a6cc0ca64bd49019d9699a06ec7f0805db31b
전하은 <hani@Hanis-Mac-Book.local>
2022-04-24 18:44:27 +0900

    [UPDATE] EstateCard 삭제 기능 구현

--- src/pages/manage-list/EstateCard.js
+++ src/pages/manage-list/EstateCard.js
@@ -2,6 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 const EstateCard = () => {
+  const deleteEstate = () => {
+    //택우는 똑똑이 -> 왜냐? 삭제해서 재랜더링 디펜던시는 그냥 fetch해온 배열의 값을 주면 되니까
+    fetch(`http://localhost:8000/delete?id=id`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(res => console.log(res));
+  };
+
+  //삭제 확인 알럿창
+  const checkDelete = () => {
+    const isDelete = window.confirm('삭제하시겠습니까?');
+    if (isDelete) {
+      deleteEstate();
+    }
+  };
+
   return (
     <Wrapper>
       <CardBox>
@@ -31,10 +51,24 @@ const EstateCard = () => {
             <div className="descBox" />
           </Description>
           <Buttons>
-            <div className="registerDate">
-              <p>등록일</p>
-              <p>조회수</p>
-              <p>찜</p>
+            <div className="buttonDiv">
+              <div className="registerDate">
+                <p>
+                  등록일 <span className="date">2022-04-23</span>
+                </p>
+                <p>
+                  조회수 <span className="count">1</span>
+                </p>
+                <p>
+                  찜 <span className="count">0</span>
+                </p>
+              </div>
+              <div className="buttons">
+                <button>수정</button>
+                <button onClick={checkDelete}>삭제</button>
+                <button>광고 종료</button>
+                <button>거래 완료</button>
+              </div>
             </div>
           </Buttons>
         </DescriptionBox>
@@ -137,5 +171,38 @@ const Description = styled.div`
 
 const Buttons = styled.div`
   width: 50%;
+  font-size: 13px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .registerDate {
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: center;
+    color: #666666;
+    p:nth-child(-n + 2) {
+      padding-right: 5px;
+      margin-right: 5px;
+      border-right: 1px solid rgb(226, 226, 226);
+
+      .count {
+        color: rgb(21, 100, 249);
+      }
+    }
+  }
+
+  .buttons {
+    margin-top: 20px;
+    button {
+      padding: 10px 15px;
+      background-color: white;
+      border: 1px solid rgb(226, 226, 226);
+      :hover {
+        cursor: pointer;
+        background-color: rgb(204, 204, 204);
+      }
+    }
+  }
 `;
 export default EstateCard;
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -4,11 +4,7 @@ import reset from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
 
   ${reset}
-<<<<<<< HEAD
-  body{
-=======
   body {
->>>>>>> ddcfdd23b1d7a28bfc90b2c863bbcfab15f9440c
     font-family: 'Spoqa Han Sans Neo', 'sans-serif';
     box-sizing: border-box;
   }
