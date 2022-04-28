a05ee4bacd7de78ecc0efef6f76a5f78118988d3
haeyun jun <haleyjun9@gmail.com>
2022-04-29 00:19:36 +0900

    [FIX] trade_type으로 변경,로컬스토리지  배열 값 20개까지 들어가는 로직 구현

--- src/pages/map-page/ListCard.js
+++ src/pages/map-page/ListCard.js
@@ -15,36 +15,27 @@ function ListCard({ data }) {
     setLike(like ? false : true);
 
     //찜 변경 API(회원만 가능) *******************************************************************
-    //프론트 로직 : 하트 클릭시 state 값 변경 및 찜변경 API에 GET요청 ***isLike 값 안줘도 구현이 가능한지 궁금***
     fetch(`http://localhost:8000/favorite/likes/${data.id}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         token: token,
       },
-      // body: JSON.stringify({
-      //   isLike: like,
-      // }),
     }).then(res => res.json());
   };
 
   const updateRecentRoom = () => {
     let arr = localStorage.getItem('recentRoom'); //로컬스토리지 값을 불러온다
-    //로컬스토리지에 데이터가 없을 경우 새로운 배열을 만들고, 데이터가 있을 경우 데이터를 풀어준다.
     if (arr === null) {
-      arr = [];
+      arr = []; //로컬스토리지에 데이터가 없을 경우 새로운 배열을 만든다.
     } else {
-      arr = JSON.parse(arr);
+      arr = JSON.parse(arr); // 데이터가 있을 경우 JSON 문자열을 자바스크립트 값으로 변환해준다.
+      arr.length === 20 ? arr.shift() : (arr = arr); //배열의 길이가 20이 되면 배열 첫 번째 값을 제거한다. 길이가 20 미만이면 변화가 없다.
     }
-    //리스트의 매물을 클릭하면 선택한 매물의 id를 배열에 저장한다.
-    arr.push(data.id);
-
-    //중복된 데이터를 넣지 않는 set 자료형에 arr를 담으면 중복이 제거된다.
-    arr = new Set(arr);
-    //중복 제거된 set 자료형의 arr를 일반 배열로 변경한다.
-    arr = [...arr];
-    //로컬스토리지에 데이터를 JSON 자료형으로 저장한다.
-    localStorage.setItem('recentRoom', JSON.stringify(arr));
+    arr.push(data.id); //리스트의 매물을 클릭하면 선택한 매물의 id를 배열에 저장한다.
+    arr = new Set(arr); //[중복 제거] : 중복된 데이터를 넣지 않는 set 자료형에 arr를 담으면 중복이 제거된다.
+    arr = [...arr]; //중복 제거된 set 자료형의 arr를 일반 배열로 변경한다.
+    localStorage.setItem('recentRoom', JSON.stringify(arr)); //로컬스토리지에 데이터를 JSON 문자열로 저장한다.
   };
 
   return (
@@ -61,7 +52,7 @@ function ListCard({ data }) {
 
               {/* 좋아요 버튼(API연결) */}
               <Like>
-                {data.hasOwnProperty('isLike') ? (
+                {data.hasOwnProperty('is_like') ? (
                   like ? (
                     <IoMdHeart color="red" onClick={() => updateLike()} />
                   ) : (
@@ -80,7 +71,7 @@ function ListCard({ data }) {
             </ImageWrapper>
             <InfoWrapper>
               <p className="price">
-                {/* {data.tradeTypes.length === 1 && data.tradeTypes[0] === '전세' //배열데이터[월세,전세] or [전세]
+                {/* {data.trade_type.length === 1 && data.trade_type[0] === '전세' //배열데이터[월세,전세] or [전세]
                   ? `전세 ${Math.floor(data.price_main / 10000)}억${
                       Math.floor(data.price_main) -
                         Math.floor(data.price_main / 10000) * 10000 ===
